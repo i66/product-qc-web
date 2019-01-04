a7f81da8ac5da9a014416438ac62b28c6ec5b696
YongZengYeo <yongzengyeo@hexsave.com>
2019-01-04 12:09:48 +0800

    #1 remove unnecessary and left create only in controller

--- product-qc-web/Controllers/TManufacturesController.cs
+++ product-qc-web/Controllers/TManufacturesController.cs
@@ -18,32 +18,6 @@ namespace product_qc_web.Controllers
             _context = context;
         }
 
-        // GET: TManufactures
-        public async Task<IActionResult> Index()
-        {
-            var hexsaveContext = _context.TManufacture.Include(t => t.ProductCodeNavigation);
-            return View(await hexsaveContext.ToListAsync());
-        }
-
-        // GET: TManufactures/Details/5
-        public async Task<IActionResult> Details(decimal? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var tManufacture = await _context.TManufacture
-                .Include(t => t.ProductCodeNavigation)
-                .FirstOrDefaultAsync(m => m.WorkOrderNum == id);
-            if (tManufacture == null)
-            {
-                return NotFound();
-            }
-
-            return View(tManufacture);
-        }
-
         // GET: TManufactures/Create
         public IActionResult Create()
         {
@@ -62,93 +36,10 @@ namespace product_qc_web.Controllers
             {
                 _context.Add(tManufacture);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(Create));
             }
             ViewData["ProductCode"] = new SelectList(_context.TProduct, "ProductCode", "ProductName", tManufacture.ProductCode);
             return View(tManufacture);
-        }
-
-        // GET: TManufactures/Edit/5
-        public async Task<IActionResult> Edit(decimal? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var tManufacture = await _context.TManufacture.FindAsync(id);
-            if (tManufacture == null)
-            {
-                return NotFound();
-            }
-            ViewData["ProductCode"] = new SelectList(_context.TProduct, "ProductCode", "ProductName", tManufacture.ProductCode);
-            return View(tManufacture);
-        }
-
-        // POST: TManufactures/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(decimal id, [Bind("ProductCode,WorkOrderNum,MachineNum")] TManufacture tManufacture)
-        {
-            if (id != tManufacture.WorkOrderNum)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(tManufacture);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!TManufactureExists(tManufacture.WorkOrderNum))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["ProductCode"] = new SelectList(_context.TProduct, "ProductCode", "ProductName", tManufacture.ProductCode);
-            return View(tManufacture);
-        }
-
-        // GET: TManufactures/Delete/5
-        public async Task<IActionResult> Delete(decimal? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var tManufacture = await _context.TManufacture
-                .Include(t => t.ProductCodeNavigation)
-                .FirstOrDefaultAsync(m => m.WorkOrderNum == id);
-            if (tManufacture == null)
-            {
-                return NotFound();
-            }
-
-            return View(tManufacture);
-        }
-
-        // POST: TManufactures/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(decimal id)
-        {
-            var tManufacture = await _context.TManufacture.FindAsync(id);
-            _context.TManufacture.Remove(tManufacture);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
 
         private bool TManufactureExists(decimal id)
