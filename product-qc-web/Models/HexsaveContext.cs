09e01639b42d9e04b8bd279cc50b6266d4d01281
YongZengYeo <yongzengyeo@hexsave.com>
2019-01-25 16:15:47 +0800

    #12 db user change name

--- product-qc-web/Models/HexsaveContext.cs
+++ product-qc-web/Models/HexsaveContext.cs
@@ -25,7 +25,7 @@ namespace product_qc_web.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=192.168.15.201\\SQLEXPRESS;Initial Catalog=Hexsave;Persist Security Info=True;User ID=hexbot;Password=54232885");
+                optionsBuilder.UseSqlServer("Data Source=192.168.15.201\\SQLEXPRESS;Initial Catalog=Hexsave;Persist Security Info=True;User ID=webuse;Password=Hex54232885");
             }
         }
 
