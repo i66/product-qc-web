44c9d2a6fae29fe5e7e49499d4c555f8b2c60b88
wunyu.huang <wunyu.huang@hexsave.com>
2019-01-25 11:55:46 +0800

    #13 add email utility

---
+++ product-qc-web/Controllers/EmailUtilitiesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace product_qc_web.Controllers
+{
+    public class EmailUtilitiesController : Controller
+    {
+        // GET: EmailUtilities
+        public ActionResult SendEmail()
+        {
+            return View();
+        }
+
+      
+    }
+}
