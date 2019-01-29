5d3971237e3aeb35ee9635227022f9210babc8d1
wunyu.huang <wunyu.huang@hexsave.com>
2019-01-29 17:23:05 +0800

    #13 refactory getFieldName functions

--- product-qc-web/Controllers/EmailUtilitiesController.cs
+++ product-qc-web/Controllers/EmailUtilitiesController.cs
@@ -1,27 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using product_qc_web.Lib;
 using product_qc_web.Models;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Linq;
 using System.Net.Mail;
 using System.Net.Mime;
-using System.Reflection;
 using System.Text;
 
 namespace product_qc_web.Controllers
 {
-    public struct CsvField
-    {
-        public const string PRODUCT_NAME = "ProductName";
-        public const string QC_FINISHED_TIME = "QcFinishedTime";
-        public const string WORK_ORDER_NUM = "WorkOrderNum";
-        public const string MACHINE_NUM = "MachineNum";
-        public const string DELIVERY_DEST = "DeliveryDestination";
-        public const string EXCHANGE_MAL_NOTE = "ExchangeReturnMalfunctionNote";
-    }
-
     public class EmailUtilitiesController : Controller
     {
         const string SMTP_SERVER = "smtp.gmail.com";
@@ -44,7 +33,7 @@ namespace product_qc_web.Controllers
                 return errorResponse("寄件人或收信人不能為空的！！");
 
             List<Type> t = new List<Type> { typeof(MetadataTDelivery), typeof(MetadataTProduct) };
-            string fieldName = concatField(getFieldName(t));
+            string fieldName = new FieldUtility().getFieldName(t);
 
             if (string.IsNullOrWhiteSpace(fieldName))
                 return errorResponse("Field不能為空！！");
@@ -124,60 +113,6 @@ namespace product_qc_web.Controllers
             }
 
             return View();
-        }
-
-        private Dictionary<string, string> getFieldName(List<Type> t)
-        {
-            if (t == null)
-                return null;
-
-            Dictionary<string, string> allFieldName = new Dictionary<string, string>();
-
-            foreach (Type item in t)
-            {
-                PropertyInfo[] props = item.GetProperties();
-                foreach (PropertyInfo prop in props)
-                {
-                    object[] attrs = prop.GetCustomAttributes(true);
-                    foreach (object attr in attrs)
-                    {
-                        DisplayAttribute field = attr as DisplayAttribute;
-                        if (field != null)
-                            allFieldName.Add(prop.Name, field.Name);
-                    }
-                }
-            }
-            return allFieldName;
-        }
-
-        private string concatField(Dictionary<string, string> allField)
-        {
-            if (allField == null || !areAllFieldExist(allField))
-                return null;
-
-            string result = allField[CsvField.PRODUCT_NAME] + "," + allField[CsvField.QC_FINISHED_TIME] + "," + 
-                            allField[CsvField.WORK_ORDER_NUM] + "," + allField[CsvField.MACHINE_NUM] + "," + 
-                            allField[CsvField.DELIVERY_DEST] + "," + allField[CsvField.EXCHANGE_MAL_NOTE];
-
-            return result;
-        }
-
-        private bool areAllFieldExist(Dictionary<string, string> allField)
-        {
-            if (!allField.ContainsKey(CsvField.PRODUCT_NAME))
-                return false;
-            if (!allField.ContainsKey(CsvField.QC_FINISHED_TIME))
-                return false;
-            if (!allField.ContainsKey(CsvField.WORK_ORDER_NUM))
-                return false;
-            if (!allField.ContainsKey(CsvField.MACHINE_NUM))
-                return false;
-            if (!allField.ContainsKey(CsvField.DELIVERY_DEST))
-                return false;
-            if (!allField.ContainsKey(CsvField.EXCHANGE_MAL_NOTE))
-                return false;
-
-            return true;
         }
 
         private IActionResult errorResponse(string errorMsg)
--- product-qc-web/Lib/FieldUtility.cs
+++ product-qc-web/Lib/FieldUtility.cs
@@ -1,11 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 
 namespace product_qc_web.Lib
 {
+    public struct CsvField
+    {
+        public const string PRODUCT_NAME = "ProductName";
+        public const string QC_FINISHED_TIME = "QcFinishedTime";
+        public const string WORK_ORDER_NUM = "WorkOrderNum";
+        public const string MACHINE_NUM = "MachineNum";
+        public const string DELIVERY_DEST = "DeliveryDestination";
+        public const string EXCHANGE_MAL_NOTE = "ExchangeReturnMalfunctionNote";
+    }
+
     public class FieldUtility
     {
+        public string getFieldName(List<Type> t)
+        {
+            if (t == null)
+                return null;
+
+            Dictionary<string, string> allFieldName = new Dictionary<string, string>();
+
+            foreach (Type item in t)
+            {
+                PropertyInfo[] props = item.GetProperties();
+                foreach (PropertyInfo prop in props)
+                {
+                    object[] attrs = prop.GetCustomAttributes(true);
+                    foreach (object attr in attrs)
+                    {
+                        DisplayAttribute field = attr as DisplayAttribute;
+                        if (field != null)
+                            allFieldName.Add(prop.Name, field.Name);
+                    }
+                }
+            }
+            return concatField(allFieldName);
+        }
+
+        private string concatField(Dictionary<string, string> allField)
+        {
+            if (allField == null || !areAllFieldExist(allField))
+                return null;
+
+            string result = allField[CsvField.PRODUCT_NAME] + "," + allField[CsvField.QC_FINISHED_TIME] + "," +
+                            allField[CsvField.WORK_ORDER_NUM] + "," + allField[CsvField.MACHINE_NUM] + "," +
+                            allField[CsvField.DELIVERY_DEST] + "," + allField[CsvField.EXCHANGE_MAL_NOTE];
+
+            return result;
+        }
+
+        private bool areAllFieldExist(Dictionary<string, string> allField)
+        {
+            if (!allField.ContainsKey(CsvField.PRODUCT_NAME))
+                return false;
+            if (!allField.ContainsKey(CsvField.QC_FINISHED_TIME))
+                return false;
+            if (!allField.ContainsKey(CsvField.WORK_ORDER_NUM))
+                return false;
+            if (!allField.ContainsKey(CsvField.MACHINE_NUM))
+                return false;
+            if (!allField.ContainsKey(CsvField.DELIVERY_DEST))
+                return false;
+            if (!allField.ContainsKey(CsvField.EXCHANGE_MAL_NOTE))
+                return false;
+
+            return true;
+        }
     }
 }
