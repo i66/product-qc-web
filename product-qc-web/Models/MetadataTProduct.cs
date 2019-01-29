e0a38153f973ee45b46c9225bd9db432c325d898
wunyu.huang <wunyu.huang@hexsave.com>
2019-01-29 16:06:59 +0800

    #13 add T_Product metadata

---
+++ product-qc-web/Models/MetadataTProduct.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace product_qc_web.Models
+{
+    [ModelMetadataType(typeof(MetadataTProduct))]
+    public partial class TProduct
+    {
+    }
+
+    public class MetadataTProduct
+    {
+        [Display(Name = "成品")]
+        public string ProductName { get; set; }
+    }
+}
