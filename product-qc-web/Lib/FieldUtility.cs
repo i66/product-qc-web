0e43dfd872eca1843327a2f83e12429e5f20a3da
wunyu.huang <wunyu.huang@hexsave.com>
2019-01-29 16:59:44 +0800

    #13 add class for get field function

---
+++ product-qc-web/Lib/FieldUtility.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace product_qc_web.Lib
+{
+    public class FieldUtility
+    {
+    }
+}
