a5d46c76e06aae11d5abc653bef241c6e2c72348
wunyu.huang <wunyu.huang@hexsave.com>
2019-01-31 14:45:38 +0800

    #16 add Emailing method

--- web-utility/ProductQC.cs
+++ web-utility/ProductQC.cs
@@ -5,10 +5,17 @@ using System;
 namespace web_utility
 {
     public partial class Frm_ProductQC : Form
-    {        
+    {
         public Frm_ProductQC()
         {
             InitializeComponent();
         }
+
+        public void Emailing(string url, string from, string recipients)
+        {
+            EmailHandling eh = new EmailHandling(webBrowser_qc);
+            eh.SendEmail(url, from, recipients);
+        }
+
     }
 }
