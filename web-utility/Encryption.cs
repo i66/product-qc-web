c47a1a2f66c798f4f80828ff897155c8ce3d0952
wunyu.huang <wunyu.huang@hexsave.com>
2019-01-31 14:39:33 +0800

    #16 move function to Encryption

--- web-utility/Encryption.cs
+++ web-utility/Encryption.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Web;
@@ -9,6 +11,44 @@ namespace web_utility
     public class Encryption
     {
         private readonly string CRYPTOKEY = DateTime.UtcNow.Date.ToString("yyyyMMdd") + "開鎖鑰匙";
+        public string PARA_ARG = "encryptMsg";
+        public string PARA_FROM = "from";
+        public string PARA_RECIPIENT = "recipients";
+        public string ASSIGNER = "=";
+        private char SEPARATOR = '&';
+
+        public string GetEncryptMsg(string from, string recipients)
+        {
+            if (string.IsNullOrWhiteSpace(from) || string.IsNullOrWhiteSpace(recipients))
+                return null;
+
+            string encryptMsg = AesEncrypt(PARA_FROM + ASSIGNER + from + SEPARATOR + PARA_RECIPIENT + ASSIGNER + recipients);
+
+            return encryptMsg;
+        }
+
+        public Dictionary<string, string> GetDecryptMsg(string encryptMsg)
+        {
+            if (string.IsNullOrWhiteSpace(encryptMsg))
+                return null;
+
+            string decryptMsg = AesDecrypt(encryptMsg);
+
+            List<string> paraList = decryptMsg.Split(SEPARATOR).ToList();
+
+            Dictionary<string, string> para = new Dictionary<string, string>();
+
+            foreach (string item in paraList)
+            {
+                string paraName = item.Substring(0, item.IndexOf(ASSIGNER));
+                string paraVal = item.Substring(item.IndexOf(ASSIGNER) + 1);
+
+                if (!para.ContainsKey(paraName))
+                    para.Add(paraName, paraVal);
+            }
+
+            return para;
+        }
 
         public string AesEncrypt(string msg)
         {
